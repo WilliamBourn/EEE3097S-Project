2f9ba6438b8bd0843f64dd12fd4b95d0d44c0b4e
Declan Molloy <iamsuperboy01@gmail.com>
2020-10-21 04:03:57 +0200

    finally it works to a mostly acceptable degree, note TODO: at the bottom

--- Lock_Pad.py
+++ Lock_Pad.py
@@ -35,16 +35,16 @@ _lock_open_en = None
 
 #Setup Functions
 #-----------------------------------------------
-def initialize_gpio(high_lock=DEFAULT_HIGHLOCK,long_press_en=False):
+def initialize_gpio(high_lock=DEFAULT_HIGHLOCK,lock_open_en=False):
     global _lock_open_en
-    
+
     #Set all the global variables
-    _lock_open_en = _lock_open_en
-            
+    _lock_open_en = lock_open_en
+
     #Set GPIO mode to board
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-        
+
     #Initialize row pins as output pins with a default high logic value
     GPIO.setup(high_lock, GPIO.OUT)
     GPIO.output(high_lock, GPIO.LOW)
@@ -52,34 +52,40 @@ def initialize_gpio(high_lock=DEFAULT_HIGHLOCK,long_press_en=False):
 #Boolean Functions
 #-----------------------------------------------
 def enable_lock_open(bool):
-    global _lock_open_en 
+    global _lock_open_en
     _lock_open_en = bool
 
 def close_lock():
+  print("about to close lock") #for testing
   GPIO.output(DEFAULT_HIGHLOCK, GPIO.HIGH)
   print("closing lock") #for testing purposes
-  
+
 def open_lock():
+  print("about to open lock") #for testing
   GPIO.output(DEFAULT_HIGHLOCK, GPIO.LOW)
   print("opening lock") #for testing purposes
-  
+
 #Main function
 #-----------------------------------------------
 #Dispose of this when done. Shouldn't be part of library, its only for testing
 
 if __name__ == "__main__":
     try:
-        initialize_gpio(_lock_open_en=True)
+        initialize_gpio(lock_open_en=True)
         while True:
-            lockstate = input("Close lock? (Y/N) "
+            lockstate = input("Close lock? (Y/N) ")
+            print("your input was: "+lockstate)
 
             if lockstate=="Y":
-                close_lock
-            elif: lockstate=="N":
-                open_lock
+                close_lock()
+            elif lockstate=="N":
+                open_lock()
             else:
                 print("invalid input")
 
 
     except Exception as e:
         print(e)
+
+#TODO: Figure out how how to make the GPIO.LOW voltage even lower so magnet isn't semi hard
+#TODO: Figure out why the code runs on a Thonny IDE but not on the commandline
