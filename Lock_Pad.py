f0ebaa5b6281958e6639d69e10b9d5b70f6d39e7
beepbeepboop <35560562+beepbeepboop@users.noreply.github.com>
2020-10-21 03:15:15 +0200

    v1. Close and open lock via command line inputs
    use pin 37(red) and pin 39(black/ground)

--- Lock_Pad.py
+++ Lock_Pad.py
@@ -3,7 +3,14 @@
 #-----------------------------------------------
 
 #Libraries used:
+#RPi.GPIO - Standard Raspberry Pi GPIO library
 
+#Try to import the libraries and exit the program if some are missing
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError as e:
+    print(e)
+    sys.exit(1)
 
 #-----------------------------------------------
 #Variables
@@ -11,12 +18,16 @@
 
 #Constants
 #-----------------------------------------------
+#Default Pins
+DEFAULT_HIGHLOCK = 37 #sets to voltage high for on, and low for off
+DEFAULT_GROUNDLOCK = 39 #always set to ground, never used
 
 #Global Variables
 #-----------------------------------------------
 
 #Flags
 #-----------------------------------------------
+_lock_open_en = None
 
 #-----------------------------------------------
 #Functions
@@ -24,6 +35,51 @@
 
 #Setup Functions
 #-----------------------------------------------
+def initialize_gpio(high_lock=DEFAULT_HIGHLOCK,long_press_en=False):
+    global _lock_open_en
+    
+    #Set all the global variables
+    _lock_open_en = _lock_open_en
+            
+    #Set GPIO mode to board
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+        
+    #Initialize row pins as output pins with a default high logic value
+    GPIO.setup(high_lock, GPIO.OUT)
+    GPIO.output(high_lock, GPIO.LOW)
 
+#Boolean Functions
+#-----------------------------------------------
+def enable_lock_open(bool):
+    global _lock_open_en 
+    _lock_open_en = bool
+
+def close_lock:
+  GPIO.output(DEFAULT_HIGHLOCK, GPIO.HIGH)
+  print("closing lock") #for testing purposes
+  
+def open_lock:
+  GPIO.output(DEFAULT_HIGHLOCK, GPIO.LOW)
+  print("opening lock") #for testing purposes
+  
 #Main function
 #-----------------------------------------------
+#Dispose of this when done. Shouldn't be part of library, its only for testing
+
+if __name__ == "__main__":
+    try:
+        initialize_gpio(_lock_open_en=True)
+        while True:
+            lockstate = input("Close lock? (Y/N) "
+            
+            if lockstate=="Y":
+                close_lock                
+            elif: lockstate=="N":
+                open_lock
+            else:
+                print("invalid input")
+
+    
+    except Exception as e:
+        print(e) 
