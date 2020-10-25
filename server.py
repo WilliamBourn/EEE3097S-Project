29fca00b4547a49106c5a66b8d85b168fff2b00f
William Bourn <willjbourn@gmail.com>
2020-10-25 21:23:08 +0000

    Added additional calls to server.py

--- server.py
+++ server.py
@@ -4,33 +4,47 @@ import EOZ_IP40 as Keypad
 import RS_Pro_150N as Lock
 import RPi.GPIO as GPIO  
 
-Request = None
-
 
 class RequestHandler_httpd(BaseHTTPRequestHandler):
+  Request = []
+  
+  maincode = []
+  tempcode = []
+  
   Lock1 = Lock.Maglock()
+  Keypad1 = Keypad.Keypad()
   
   def do_GET(self):
-    global Request
     messagetosend = bytes('Hello world',"utf")
     self.send_response(200)
     self.send_header('Content-Type', 'text/plain')
     self.send_header('Content-Length', len(messagetosend))
     self.end_headers()
     self.wfile.write(messagetosend)
-    Request = self.requestline
+    self.Request = self.requestline
     Request = Request[5 : int(len(Request)-9)]
     
     print(Request)
     
-    if Request == 'on':
+    if self.Request == 'on':
       Lock1.activate_lock() #should activate lock
       print(Lock1.lock_pin) #testing the lock pin
 
-    if Request == 'off':
+    if self.Request == 'off':
       Lock1.deactivate_lock()
-    return
 
+    code = self.Request[int(len(Request)-5):]    
+    print(code)
+    self.Request = self.Request[:int(len(Request)-5)]
+    print(self.Request)
+
+    if self.Request == 'setMain':
+      self.maincode = code
+    
+    if self.Request == 'setTemp':
+      self.tempcode = code
+
+    return
 
 server_address_httpd = ('192.168.0.120',8080)
 httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
